11b5cfe4326d6d38d120025aa88f9cc7066bcc91
TinhNT <tinh.nt@namitech.io>
2024-10-30 21:39:59 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>
+        Click me
+      </Button>
+    </div>
+  );
+}
